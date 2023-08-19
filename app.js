b0486e86978effbeb04b2435daa667a0b7b3d12c
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-19 20:22:38 +0330

    set default middleware

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+// routes
+const authRoutes = require('./routes/v1/auth');
 
 const app = express();
+app.use('/courses/covers', express.static(path.join(__dirname, 'public', 'courses', 'covers')));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use("/v1/auth", authRoutes);
 
 module.exports = app;
---
+++ controllers/v1/auth.js
@@ -0,0 +1,7 @@
+const UserModel = require('../../models/user');
+
+exports.register = async (req, res, next) => {};
+
+exports.login = async (req, res, next) => {};
+
+exports.getMe = async (req, res, next) => {};
