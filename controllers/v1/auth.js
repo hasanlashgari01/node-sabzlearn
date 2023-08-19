f47bed2dd4185ad70232f78c18441dc842bfd2bf
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-19 21:35:28 +0330

    user can register

--- controllers/v1/auth.js
+++ controllers/v1/auth.js
@@ -1,7 +1,49 @@
-const UserModel = require('../../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const userModel = require('../../models/user');
 
-exports.register = async (req, res, next) => {};
+const registerValidator = require('../../validators/register');
 
-exports.login = async (req, res, next) => {};
+exports.register = async (req, res, next) => {
+    const validationResults = registerValidator(req.body);
 
-exports.getMe = async (req, res, next) => {};
+    if (validationResults !== true) {
+        return res.status(422).json(validationResults);
+    }
+
+    const {username, name, email, password, phone} = req.body;
+
+    const isUserExist = await userModel.findOne({$or: [{username}, {email}]});
+
+    if (isUserExist) {
+        return res.status(409).json({
+            message: 'User already exists'
+        });
+    }
+
+    const countOfUsers = await userModel.count();
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await userModel.create({
+        username,
+        name,
+        email,
+        password: hashedPassword,
+        phone,
+        role: countOfUsers === 0 ? 'ADMIN' : 'USER'
+    });
+
+    const userObject = user.toObject();
+    Reflect.deleteProperty(userObject, 'password');
+
+    const accessToken = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '30 days'});
+
+    return res.status(201).json({user: userObject, accessToken});
+};
+
+exports.login = async (req, res, next) => {
+};
+
+exports.getMe = async (req, res, next) => {
+};
