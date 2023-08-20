fadb55ef3de56feea4fbf2c3e1ed4ab092cb1360
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-20 18:38:06 +0330

    feat: ban user

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 // routes
 const authRoutes = require('./routes/v1/auth');
+const userRoutes = require('./routes/v1/user');
 
 const app = express();
 app.use('/courses/covers', express.static(path.join(__dirname, 'public', 'courses', 'covers')));
@@ -12,5 +13,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use("/v1/auth", authRoutes);
+app.use("/v1/users", userRoutes);
 
 module.exports = app;
---
+++ controllers/v1/user.js
@@ -0,0 +1,15 @@
+const UserModel = require('../../models/user')
+const banUserModel = require('../../models/ban-phone')
+
+exports.banUser = async (req, res) => {
+    const mainUser = await UserModel.findOne({ _id: req.params.id }).lean()
+    const banUserResult = await banUserModel.create({ phone: mainUser.phone })
+
+    if (banUserResult) {
+        return res.status(200).json({
+            message: 'User has been banned',
+        })
+    }
+
+    return res.status(500).json({ message: 'Something went wrong' })
+}
