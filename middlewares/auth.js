a1a9bf395710628fdde046213b253f33f27a8333
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-20 19:47:11 +0330

    auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/user')
+
+module.exports = async (req, res, next) => {
+    const authHeader = req.header('Authorization').split(' ')[1]
+
+    if (authHeader.length !== 2) {
+        return res.status(403).json({ message: 'This route is protected and you can\'t have access to it' })
+    }
+
+    const token = authHeader[1]
+
+    try {
+        const jwtPayload = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findById(jwtPayload._id).lean();
+
+        Reflect.deleteProperty(user, 'password');
+
+        req.user = user;
+
+        next()
+    } catch (error) {
+        return res.json(error)
+    }
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require('./app');
-const {default: mongoose} = require('mongoose');
-require('dotenv').config();
+const app = require('./app')
+const { default: mongoose } = require('mongoose')
+require('dotenv').config()
 
 const port = process.env.PORT || 4000;
 
@@ -8,14 +8,19 @@ const port = process.env.PORT || 4000;
 (async () => {
     await mongoose.connect(process.env.MONGO_URL)
         .then(() => {
-            console.log('Connected to MongoDB');
+            console.log('Connected to MongoDB')
         })
         .catch((err) => {
-            console.log(err);
+            console.log(err)
         })
-})();
+})()
+
+app.get('/', (req, res) => {
+    console.log("Token =>",req.header('Authorization').split(' ')[1]);
+    res.json({ message: 'Welcome to the server' })
+})
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+    console.log(`Server listening on port ${port}`)
+})
