d56104c2c53f3c28865d097f454af6efe250e450
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-23 23:15:46 +0330

    create model

---
+++ controllers/v1/course.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    cover: {
+        type: String,
+        required: true,
+    },
+    support: {
+        type: String,
+        required: true,
+    },
+    href: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['draft', 'published'],
+        default: 'draft',
+    },
+    discount: {
+        type: Number,
+        required: true,
+    },
+    categoryID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true,
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+}, { timestamps: true })
+
+schema.virtual('sessions', {
+    ref: 'Session',
+    localField: '_id',
+    foreignField: 'course',
+})
+
+schema.virtual('comments', {
+    ref: 'Comment',
+    localField: '_id',
+    foreignField: 'course',
+})
+
+const model = mongoose.model('Course', schema)
+
+module.exports = model
---
+++ models/comment.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    body: {
+        type: String,
+        required: true,
+    },
+    course: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Course',
+        required: true,
+    },
+    creator: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    isAccepted: {
+        type: Number,
+        default: 0,
+    },
+    score: {
+        type: Number,
+        default: 5,
+    },
+    isAnswer: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    mainComment: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Comment',
+    },
+}, { timestamps: true })
+
+const model = mongoose.model('Comment', schema)
+
+module.exports = model
