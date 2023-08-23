589f4651c7a89e40b6d76b41c4db69e96382aa4c
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-23 23:23:46 +0330

    create uploader

---
+++ utils/uploader.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const multer = require('multer')
+
+module.exports = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '..', 'public', 'courses', 'covers'))
+    }, filename: (req, file, cb) => {
+        // Random way
+        // const fileName = Date.now() + String(Math.random() * 9999)
+        // Hash way
+        const hashFileName = crypto.createHash('sha256').update(file.originalname).digest('hex')
+
+        const ext = path.extname(file.originalname)
+        
+        // cb(null, fileName + ext)
+        cb(null, hashFileName + ext)
+    },
+})
